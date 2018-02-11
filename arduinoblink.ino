af61d85bc479ab373d67052d039b867093ec3cea
MiguelRomero <contacto@miguelromero.mx>
2018-02-10 18:01:04 -0600

    Comentarios

--- arduinoblink.ino
+++ arduinoblink.ino
@@ -1,15 +1,12 @@
 // Arduino blink
 
 void setup(){
-	pinMode(13, OUTPUT);
-
+	pinMode(13, OUTPUT);  // Configuramos el pin 13 como salida
 }
 
 void loop (){
-
-digitalWrite(13, HIGH);
-delay(500);
-digitalWrite(13, LOW);
-delay(500);
-
+	digitalWrite(13, HIGH); // Colocamos el pin 13 en estado Alto
+	delay(500);				// Pausa de 500ms
+	digitalWrite(13, LOW);	// Colocamos el pin 13 en estado Bajo
+	delay(500);				// Pausa de 500ms
 }
